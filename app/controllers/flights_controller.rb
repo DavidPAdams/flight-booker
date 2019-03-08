89e34fb18d9b2d5a1eff59ae90f125f59daedb6e
David Adams <david.riding.rails@gmail.com>
2019-03-07 19:56:47 -0700

    Start flight search form

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,4 +1,11 @@
 class FlightsController < ApplicationController
   def index
+    
   end
+
+  private
+
+    def flight_params
+      params.require(:flight).permit(:departure_airport_id, :arrival_airport_id, :departure_time, :num_passengers)
+    end
 end
