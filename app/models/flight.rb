5f7622bf3c50f80cfc36677dd8825de8a9d1c460
David Adams <david.riding.rails@gmail.com>
2019-03-11 17:12:20 -0700

    Update for flight model

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,10 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: "Airport"
   belongs_to :arrival_airport, class_name: "Airport"
+
+  
+
+  def departure_formatted
+    departure_time.strftime("%a %d %b %Y at %I:%M %P")
+  end
 end
