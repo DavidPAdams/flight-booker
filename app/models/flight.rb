8a050b56a6981e3606586b6643a872b8eac1941a
David Adams <david.riding.rails@gmail.com>
2019-03-13 11:47:12 -0700

    Update Flight model methods

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,9 +2,14 @@ class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: "Airport"
   belongs_to :arrival_airport, class_name: "Airport"
 
-  
-
-  def departure_formatted
-    departure_time.strftime("%a %d %b %Y at %I:%M %P")
+  def self.available_flights(depart_from, arrive_to)
+    Flight.where(departure_airport_id: depart_from)
+          .where(arrival_airport_id: arrive_to)
   end
+
+  def self.date_list(depart, arrive)
+    dates = Flight.available_flights(depart, arrive).order(departure_time: :asc)
+    dates.map {|n| n.departure_time.strftime("%a %d %b %Y at %I:%M %P") }
+  end  
+
 end
