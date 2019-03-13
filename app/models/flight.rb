1db2c5db4b59385ca6dbeeb2b6c8bb0b131c9c9b
David Adams <david.riding.rails@gmail.com>
2019-03-13 14:54:25 -0700

    Update Flight model for associations with Reservations and Passengers

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,6 +1,8 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: "Airport"
   belongs_to :arrival_airport, class_name: "Airport"
+  has_many :reservations
+  has_many :passengers, through: :reservations
 
   def self.available_flights(depart_from, arrive_to)
     Flight.where(departure_airport_id: depart_from)
@@ -10,6 +12,6 @@ class Flight < ApplicationRecord
   def self.date_list(depart, arrive)
     dates = Flight.available_flights(depart, arrive).order(departure_time: :asc)
     dates.map {|n| n.departure_time.strftime("%a %d %b %Y at %I:%M %P") }
-  end  
+  end
 
 end
