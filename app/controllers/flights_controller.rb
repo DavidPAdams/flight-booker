0bdcba626e7de930313b85b78eb2c303dd9fb9b3
David Adams <david.riding.rails@gmail.com>
2019-03-15 17:48:19 -0700

    Update flights controller for passenger count

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -5,7 +5,7 @@ class FlightsController < ApplicationController
     @passenger_choices = (1..4).map{ |p| ["#{p} " + 'person'.pluralize(p), p] }
     @departure_airport = params[:departure_airport_id]
     @arrival_airport = params[:arrival_airport_id]
-    @passenger_count = params[:passenger_count]
+    @passenger_count = params[:passenger_count].to_i
     @depart_date = params[:depature_time]
     @flights = Flight.available_flights(params[:departure_airport_id], params[:arrival_airport_id]).order(departure_time: :asc)
     @flight_dates = Flight.date_list(params[:departure_airport_id], params[:arrival_airport_id])
